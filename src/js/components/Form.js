ee5cc513fd15773c32150b0d0e8461b953d16d79
Dylan Manley <dylan.manley@gmail.com>
2020-02-29 02:37:31 -0800

    add styling to new clothing form

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { addArticle } from "../actions/index";
+import './../../styles/forms.css';
 
 function mapDispatchToProps(dispatch) {
   return {
@@ -12,7 +13,11 @@ class ConnectedForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: ""
+      title: "",
+      author: "",
+      publishedDate: "",
+      articleBody: "",
+      image: ""
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -24,20 +29,53 @@ class ConnectedForm extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    const { title } = this.state;
-    this.props.addArticle({ title });
-    this.setState({ title: "" });
+    const { title, author, publishedDate, articleBody, image } = this.state;
+    this.props.addArticle({ title, author, publishedDate, articleBody, image });
+    this.setState({ title: "", author: "", publishedDate: "", articleBody: "", image: "" });
   }
   render() {
-    const { title } = this.state;
+    const { title, author, publishedDate, articleBody, image } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
-        <div>
-          <label htmlFor="title">Title</label>
+        <div className="newArticleForm">
+          <div className="formLable"><label htmlFor="title">Title</label></div>
           <input
             type="text"
             id="title"
             value={title}
+            className='formInput'
+            onChange={this.handleChange}
+          />
+          <div className="formLable"><label htmlFor="author">Author</label></div>
+          <input
+            type="text"
+            id="author"
+            value={author}
+            className='formInput'
+            onChange={this.handleChange}
+          />
+          <div className="formLable"><label htmlFor="publishedDate">publishedDate</label></div>
+          <input
+            type="text"
+            id="publishedDate"
+            value={publishedDate}
+            className='formInput'
+            onChange={this.handleChange}
+          />
+          <div className="formLable"><label htmlFor="articleBody">Article Body</label></div>
+          <input
+            type="text"
+            id="articleBody"
+            value={articleBody}
+            className='formInput'
+            onChange={this.handleChange}
+          />
+          <div className="formLable"><label htmlFor="image">image</label></div>
+          <input
+            type="text"
+            id="image"
+            value={image}
+            className='formInput'
             onChange={this.handleChange}
           />
         </div>

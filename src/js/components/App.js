c53115ea57de38522acb2ab5e17ceb85a6514b2d
Dylan Manley <dylan.manley@gmail.com>
2020-02-29 02:12:53 -0800

    play around with color scheme for backgrounds"

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import List from "./List";
 import Form from "./Form";
+import './../../styles/index.css';
 
 const App = () => (
   <>
-    <div>
-      <h2>Articles</h2>
-      <List />
-    </div>
-    <div>
-      <h2>Add a new article</h2>
-      <Form />
+    <div className="body">
+      <div className="topAndBottom">
+        <div className="topBody">
+          <h2>Articles</h2>
+          <List />
+        </div>
+        <div className="bottomBody">
+          <h2>Add a new article</h2>
+          <Form />
+        </div>
+      </div>
     </div>
   </>
 );

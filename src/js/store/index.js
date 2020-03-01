c8b5f00709d59dd33080bfc3dc393f1012f86527
Dylan Manley <dylan.manley@gmail.com>
2020-02-29 16:02:10 -0800

    add saturday scheduel

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,6 +1,9 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/index";
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  applyMiddleware(forbiddenWordsMiddleware)
+);
 
 export default store;

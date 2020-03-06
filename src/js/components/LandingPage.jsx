6ce2c095920260bc63d387eaa1e12766e2fccf9c
Dylan Manley <dylan.manley@gmail.com>
2020-03-05 20:37:18 -0800

    Add links and some styling to landing page

--- src/js/components/LandingPage.jsx
+++ src/js/components/LandingPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import start from '../../images/start.png';
+import './../../styles/landingPage.css'
 
 function LandingPage(){
   return (
   <div id='landingPage'>
-    <img className='startImg' src={start}/>
-    <img className='startImg' src={start}/>
-    <img className='startImg' src={start}/>
+    <div className='startImgs'>
+      <a href='/'><img className='startImg' src={start}/></a>
+      <a href='#/home'><img className='startImgBtn' src={start}/></a>
+      <a href='/'><img className='startImg' src={start}/></a>
+    </div>
   </div>
   );
 }

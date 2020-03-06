c4f5460fafa0ab737c6edfcaa0f9cbf39f5e2ae1
Dylan Manley <dylan.manley@gmail.com>
2020-03-05 17:50:43 -0800

    Add Routing to App.js

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -6,25 +6,28 @@ import './../../styles/index.css';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import constants from './../constants';
+import { Switch, Route } from 'react-router-dom';
 const { c } = constants;
 
 const App = () => (
   <>
     <div className="body">
+    <Switch>
     <Header />
-      <h1>body</h1>
-      <div className="topAndBottom">
-        <div className="topBody">
-          <h2>topBody</h2>
-        </div>
-        <div className="bottomBody">
-          <h2>bottomBody</h2>
-        </div>
-        <div className="topBody">
-          <h2>topBody</h2>
-        </div>
-      </div>
-      <Footer />
+    <Footer />
+    </Switch>
+    <h1>body</h1>
+    <div className="topAndBottom">
+    <div className="topBody">
+    <h2>topBody</h2>
+    </div>
+    <div className="bottomBody">
+    <h2>bottomBody</h2>
+    </div>
+    <div className="topBody">
+    <h2>topBody</h2>
+    </div>
+    </div>
     </div>
   </>
 );

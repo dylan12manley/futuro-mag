f949b5db3ca081e59df8b95c63d8d5e0750e5fff
Dylan Manley <dylan.manley@gmail.com>
2020-03-05 21:56:29 -0800

    Add styleing to footer

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import "./../../styles/footer.css"
 
 function Footer(){
   return (
-    <div id="footer">
-      <p><span className='navP'>4003 SE 42nd Ave. Portland Or. 97212.  503-555-5555</span></p>
-
+    <div >
+      <p id="footer"><span className='navP'>4003 SE 42nd Ave. Portland Or. 97212.  503-555-5555</span></p>
     </div>
   );
 }

764e6f343944616a7a02ac50ded880512593eb0a
Dylan Manley <dylan.manley@gmail.com>
2020-03-05 21:25:53 -0800

    ADD IMAGES AND STYLING TO ADD COLUMN

--- src/js/components/AdColumn.jsx
+++ src/js/components/AdColumn.jsx
@@ -2,18 +2,35 @@ import React from "react"
 import Image from 'react-bootstrap/Image'
 import dior from './../../images/dior.jpg'
 import keene from './../../images/keene.jpg'
-// import "../styles/AdColumn.css"
+import arTecHouse from './../../images/artechouse.png'
+import taschen from './../../images/Taschen.png'
+import monotoneBar1 from './../../images/monoLine1.png'
+import "./../../styles/adColumn.css"
 
 const adList = [
   {
-    title: 'ad number 1',
-    url: 'https://www.dior.com/en_us',
-    img: dior
-  },
-  {
     title: 'ad number 2',
     url: 'https://www.keane-eyes.com/',
-    img: keene
+    img: keene,
+    id: 1
+  },
+  {
+    title: 'ad number 3',
+    url: 'https://www.dior.com/en_us',
+    img: taschen,
+    id: 2
+  },
+  {
+    title: 'ad number 1',
+    url: 'https://www.dior.com/en_us',
+    img: dior,
+    id: 3
+  },
+  {
+    title: 'ad number 4',
+    url: 'https://www.keane-eyes.com/',
+    img: arTecHouse,
+    id: 4
   }
 ]
 
@@ -21,10 +38,11 @@ function AdColumn(){
   return (
   <>
   <div className="adColumnBody">
+  <Image src={monotoneBar1}/>
     {adList.map((ad, index) =>
-    <a href={ad.url}>
-      <Image src={ad.img}/>
-    </a>
+      <a href={ad.url}>
+        <Image src={ad.img}/>
+      </a>
   )}
   </div>
   </>

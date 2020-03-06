8c3ff0d08d5f3d4ad9e4ddc12b32b4f622453c36
Dylan Manley <dylan.manley@gmail.com>
2020-03-05 18:05:01 -0800

    Add image to landing page

---
+++ src/js/components/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import start from '../../images/start.png';
+
+function LandingPage(){
+  return (
+  <div id='LandingPage'>
+    <img className='startImg' src={start}/>
+  </div>
+  );
+}
+
+
+export default LandingPage;

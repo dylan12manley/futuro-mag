dbe48d459bc6a0abcee6fd202cd0aeeb21a5cad2
Dylan Manley <dylan.manley@gmail.com>
2020-03-06 09:00:42 -0800

    add images and textshadow to caoursel

--- src/js/components/CarouselHome.jsx
+++ src/js/components/CarouselHome.jsx
@@ -2,22 +2,39 @@ import React from 'react'
 import Carousel  from 'react-bootstrap/Carousel';
 import triangles1 from './../../images/tri1.png'
 import futuro1 from './../../images/Futuro1.jpg'
+import futuro2 from './../../images/Futuro2.jpg'
+import futuro3 from './../../images/Futuro3.jpg'
+import futuro4 from './../../images/Futuro4.jpg'
 import "./../../styles/carousel1.css"
 
 const featuredArticles = [
   {
-    title: 'First Story Title',
+    title: 'First Article Title',
     author: 'First Author',
     publishDate: 'March 3rd, 2020',
     articleTeaser: 'This is a story teaser. It could be a brief description of the article or a just the first couple senteneces.',
     mainImage: futuro1
   },
   {
-    title: 'Seccond Story Title',
+    title: 'Seccond Article Title',
     author: 'Seccond Author',
     publishDate: 'March 2nd, 2020',
     articleTeaser: 'This is a story teaser. It could be a brief description of the article or a just the first couple senteneces.',
-    mainImage: triangles1
+    mainImage: futuro2
+  },
+  {
+    title: 'Third Article Title',
+    author: 'Third Author',
+    publishDate: 'March 2nd, 2020',
+    articleTeaser: 'This is a story teaser. It could be a brief description of the article or a just the first couple senteneces.',
+    mainImage: futuro3
+  },
+  {
+    title: 'Fourth Article Title',
+    author: 'Fourth Author',
+    publishDate: 'March 2nd, 2020',
+    articleTeaser: 'This is a story teaser. It could be a brief description of the article or a just the first couple senteneces.',
+    mainImage: futuro4
   }
 ]
 
@@ -36,10 +53,12 @@ function  CarouselHome(){
         />
       </div>
         <Carousel.Caption>
-          <h3>{article.title}</h3>
-          <h3>{article.author}</h3>
-          <p>{article.publishDate}</p>
-          <p>{article.articleTeaser}</p>
+          <div className='carouselBody'>
+            <h1>{article.title}</h1>
+            <h3>{article.author}</h3>
+            <p>{article.publishDate}</p>
+            <p>{article.articleTeaser}</p>
+          </div>
         </Carousel.Caption>
       </Carousel.Item>
     )};

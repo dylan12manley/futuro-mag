a5b55539312ac0783ae6ce43b227f05cc623e9aa
Dylan Manley <dylan.manley@gmail.com>
2020-03-06 22:02:19 -0800

    something

--- src/js/components/MainArticleFeed.jsx
+++ src/js/components/MainArticleFeed.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { Card, Row, Col } from 'react-bootstrap';
 import tyler from './../../images/tyler.jpg';
+import rick from './../../images/RickFuturo.jpg';
 import "./../../styles/mainArticlesFeed.css"
 
 const fullArticlesList = [
+  {
+    title: 'Why is Rick so Sick, fo Real tho?',
+    author: 'Joey P',
+    publishDate: 'Febuary 19, 2020',
+    articleTeaser: "YO , look Rick and Michelle are like Futuro's  favorite people besides Elon and Grimes lol. Wait Why? Because self actualization isn't even a fucking thought , It just is mothafuckafucking thing. Like just waking up , opening your eyes and just doing shit , no negative self talk , no barriers , no fucks given.",
+    mainImage: rick
+  },
   {
     title: 'First Story Title',
     author: 'First Author',
@@ -57,7 +65,7 @@ function MainArticleFeed(){
       <Card.ImgOverlay>
         <Card.Text>
           <div className="articleSubtitle" >
-          <p>{article.articleTeaser}</p>
+          <p className='ArticleTeaserOnCard'>{article.articleTeaser}</p>
           </div>
         </Card.Text>
       </Card.ImgOverlay>

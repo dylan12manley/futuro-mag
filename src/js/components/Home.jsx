c03e877c30d16224e258cc57ec79d8ac03cadc93
Dylan Manley <dylan.manley@gmail.com>
2020-03-11 09:50:53 -0700

    remove nav bar from home

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,16 +3,12 @@ import Header from './Header'
 import Footer from './Footer'
 import MainArticleFeed from './MainArticleFeed'
 import AdColumn from './AdColumn'
-import NavBar from './NavBar2'
 import CarouselHome from './CarouselHome'
 import { Col, Row } from 'react-bootstrap'
 
 function Home(){
   return (
   <div id='home'>
-    <div id='navBar'>
-      <NavBar/>
-    </div>
     <Header/>
     <CarouselHome/>
     <Row>

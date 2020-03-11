554dc04dfd2f37affb987232185dca6f7a3adb38
Dylan Manley <dylan.manley@gmail.com>
2020-03-11 14:20:56 -0700

    add hard coded articles to mainArticleFeed

--- src/js/components/MainArticleFeed.jsx
+++ src/js/components/MainArticleFeed.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Card, Row, Col } from 'react-bootstrap';
 import tyler from './../../images/tyler.jpg';
+import kenith from './../../images/kenith.jpg';
 import rick from './../../images/RickFuturo.jpg';
+import stephen1 from './../../images/stephen1.jpg';
 import "./../../styles/mainArticlesFeed.css"
 
 const fullArticlesList = [
@@ -13,18 +15,18 @@ const fullArticlesList = [
     mainImage: rick
   },
   {
-    title: 'First Story Title',
-    author: 'First Author',
-    publishDate: 'March 3rd, 2020',
-    articleTeaser: 'This is a story teaser. It could be a brief description of the article or a just the first couple senteneces.',
-    mainImage: tyler
+    title: 'Kenneth Cappello’s Photography is Flithy',
+    author: 'Joey P',
+    publishDate: 'Febuary 19th, 2020',
+    articleTeaser: 'Kenneth Cappelo’s photography makes me feel a certain type of way , I think its the way he captures the artist in there natural essence but also makes them look dope as fuck. You have have cunt’s take lame pictures of you because they be kinda hating on the Low lol , That’s not Kenneth Cappelo , he’s def not a hater with camera. He’s wants to capture your energy and see you win , gotta love a mothafucka like that. Can def see why the stars love him. Tip , don’t give mofo’s the camera that aint trying to see you win. Real Talk.',
+    mainImage: kenith
   },
   {
-    title: 'Seccond Story Title',
-    author: 'Seccond Author',
-    publishDate: 'March 2nd, 2020',
-    articleTeaser: 'This is a story teaser. It could be a brief description of the article or a just the first couple senteneces.',
-    mainImage: tyler
+    title: 'Stephen Gibbs Brain is Melting',
+    author: 'Joey P',
+    publishDate: 'Febuary 19th, 2020',
+    articleTeaser: 'Is it me or is Stephen Gibbs really into Psychedelics or maybe he’s just a trippy dude but either way his work has a fucking place here.  The fact that him and the Homey trippy Redd hooked up for and album cover is ridiculously cool and trippy unto itself. These first few artist that we feature in Art now where not going to say too much about because there work speaks fr itself. Have a tab , put on some Nicholas Jarr and watch his world come to life.',
+    mainImage: stephen1
   },
   {
     title: 'Third Story Title',
